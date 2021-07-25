a6d84e507c557a0827a4381d27de3e2bc9daf317
Lazytangent <pm123633@gmail.com>
2021-07-24 20:06:14 -0500

    Add remote checking conditional

--- run.sh
+++ run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 cd ~
+
 mkdir -p appacademy
 cd appacademy
+
 if [ ! -d Module-1-Resources ]; then
   git clone https://github.com/appacademy/Module-1-Resources.git
 else
   cd Module-1-Resources
-  git pull
+
+  remote=$(git remote)
+  if [[ $remote == origin ]]; then
+    git pull
+  else
+    git remote add origin https://github.com/appacademy/Module-1-Resources.git
+    git pull --set-upstream origin main
+  fi
+
   cd ../
 fi
 
